05b500fdd127fe29a9b5825ebd1d7ef7cd859326
Guest User <Guest@902-25.lmusd.org>
2018-05-29 12:30:37 -0700

    ugh

--- Assets/scripts/platBot.cs
+++ Assets/scripts/platBot.cs
@@ -5,31 +5,46 @@ using UnityEngine;
 public class platBot : MonoBehaviour {
 	private GameObject plat;
 	private GameObject ball;
+	private GameObject omb;
 	private float hb;
 	private float hp;
-	float d;
+	private float ho;
+	private float vo;
+	float d,d2,d3;
+	float moveAmt;
 
 	// Use this for initialization
 	void Start () {
 		plat = GameObject.Find("mainPlatform");
 		ball = GameObject.Find ("ball");
+		omb = GameObject.Find ("bombOmb");
 	}
 
 	// Update is called once per frame
 	void Update () {
 		hb = ball.transform.position.x;
 		hp = plat.transform.position.x;
+		ho = omb.transform.position.x;
+		vo = omb.transform.position.y;
 		d = hb - hp;
-		// If we're close enough to catch the ball
-		// OR the ball is too far to catch, do nothing
-		if ((d < 2.5 && d > -2.5) || (d > 15 && pointSys.points > 300)) {
-			;
-		}
-		else if (d > 0) {
+		d2 = ho - hp;
+		d3 = vo + 4.1f;
+		moveAmt = 2.5f + 0.01f * pointSys.points;
+
+		if (d2 < 3 && d2 > -3 && d3 < moveAmt && d3 > -1) {
 			plat.transform.Translate (5 * 0.1f, 0f, 0f);
-		}
-		else if (d < 0) {
-			plat.transform.Translate (-5 * 0.1f, 0f, 0f);
+		} else if ((d3 < 2.5 && d3 > -1) && ((d2 > 3 && d2 < 7) || (d2 < -3 && d2 > -7))) {
+			;
+		} else {
+			// If we're close enough to catch the ball
+			// OR the ball is too far to catch, do nothing
+			if ((d < 2.5 && d > -2.5) || (d > 15 && pointSys.points > 300)) {
+				;
+			} else if (d > 0) {
+				plat.transform.Translate (5 * 0.1f, 0f, 0f);
+			} else if (d < 0) {
+				plat.transform.Translate (-5 * 0.1f, 0f, 0f);
+			}
 		}
 	}
 }
