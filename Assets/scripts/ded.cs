3a5a029c972ab7094e645dbd1cd23dca45a26111
echo <echo@localhost.localdomain>
2018-05-31 06:56:51 -0700

    upd

--- Assets/scripts/ded.cs
+++ Assets/scripts/ded.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class ded : MonoBehaviour {
 	// References to sounds
 	public AudioSource MissSound;
+	public AudioSource MissSound2;
+	public AudioSource MissSound3;
+	public AudioSource MissSound4;
+	public AudioSource MissSound5;
 	// Use this for initialization
 	void Start () {
 		MissSound = GetComponent<AudioSource> ();
+		MissSound2 = GameObject.Find("Canvas").GetComponent<AudioSource> ();
+		MissSound3 = GameObject.Find("Text").GetComponent<AudioSource> ();
+		MissSound4 = GameObject.Find("Sun").GetComponent<AudioSource> ();
+		MissSound5 = GameObject.Find("special").GetComponent<AudioSource> ();
 	}
 
 	// Update is called once per frame
@@ -20,7 +28,17 @@ public class ded : MonoBehaviour {
 			if (--pointSys.points <= 0)
 			pointSys.points = 0;
 			platformController.teleportBall ();
-			MissSound.Play ();
+			float r = Random.Range(0,10);
+			if (r <= 2)
+				MissSound.Play ();
+			else if (r <= 4)
+				MissSound2.Play ();
+			else if (r <= 6)
+				MissSound3.Play ();
+			else if (r <= 8)
+				MissSound4.Play ();
+			else
+				MissSound5.Play ();
 		} else {
 			platformController.teleportBomb ();
 		}
